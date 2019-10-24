4f465a619e23433fcecd8f0141e07607bb4ca9be
glshrz <glebsuccr@gmail.com>
2019-10-24 14:51:11 +0300

    add test data for screen size checking

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    body = document.body;
+    html = document.documentElement;
+    contentHeight: any;
+    equalScreenheight: boolean;
+    windowHeight: any;
 
   constructor() {}
-
+  ionViewWillEnter() {
+      this.getContentHeight();
+      this.compareBodyAndContent();
+  }
+  getWindowHeight( ) {
+      this.windowHeight = window.innerHeight;
+      console.log(this.windowHeight);
+  }
+  getContentHeight() {
+      this.contentHeight = this.body.offsetHeight;
+      console.log(this.contentHeight);
+  }
+  compareBodyAndContent() {
+      this.getContentHeight();
+      this.getWindowHeight();
+      if (this.windowHeight === this.contentHeight ) {
+          this.equalScreenheight = true;
+      } else {
+          this.equalScreenheight = false;
+      }
+  }
 }
